50b8fb86f025878b469d650eac99cfe0c491ccc8
emon2021 <mdemonahmed2021@gmail.com>
2024-07-18 00:58:42 +0600

    users table creation and migration done

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'fname','lname',
         'email',
         'password',
+        'mobile','otp'
     ];
 
     /**
